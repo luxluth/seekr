22bb1febe672b6d334a43c88fb1ba4eb0dde07c7
luxluth <delphin.blehoussi93@gmail.com>
2024-09-09 19:32:05 +0200

    Icon search cache

--- src/resources.rs
+++ src/resources.rs
@@ -1,11 +1,16 @@
 use fragile::Fragile;
 use gtk::{gdk_pixbuf::Pixbuf, gio::Icon};
 use lazy_static::lazy_static;
-use std::sync::Arc;
+use std::collections::HashMap;
+use std::path::PathBuf;
+use std::sync::{Arc, RwLock};
 
+// inspired from https://github.com/aeghn/rglauncher/blob/2789af0c36f5929a448807584aaaf57685162891/crates/rglauncher-gtk/src/iconcache.rs#L12
 lazy_static! {
     pub static ref DIVIDE_ICON: Arc<Fragile<Icon>> =
         load_svg_resource(include_bytes!("assets/divide.svg"), (24, 24));
+    pub static ref ICON_MAP: Arc<Fragile<RwLock<HashMap<String, Option<PathBuf>>>>> =
+        Arc::new(Fragile::new(RwLock::new(HashMap::new())));
 }
 
 pub fn load_svg_resource(content: &[u8], size: (i32, i32)) -> Arc<Fragile<Icon>> {
